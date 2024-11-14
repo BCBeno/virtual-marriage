c3d6bf462875e4b27f6f9ed1dae6427afc24167b
Beno <circiubeni@gmail.com>
2024-11-14 19:17:15 +0200

    first commit

---
+++ src/main/java/com/beno/virtualmarriage/VirtualMarriageApplication.java
@@ -0,0 +1,13 @@
+package com.beno.virtualmarriage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualMarriageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualMarriageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beno/virtualmarriage/VirtualMarriageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beno.virtualmarriage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualMarriageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
